a62681c9cc6b8aa30de11d9b7ad7cf28341e499b
Ismail Syarief <ismail8syarief@gmail.com>
2019-07-19 10:38:59 +0700

    add axios

--- App.js
+++ App.js
@@ -1,62 +1,53 @@
 import React from "react";
 import { FlatList } from "react-native";
-import { Text, ListItem, Left, Body, Icon, Right, Title, Container, Content, Item, Input, List, Thumbnail } from "native-base";
+import axios from 'axios';
+import { Text, ListItem, Left, Body, Icon, Right, Title, Container, Content, Item, Input, List, Thumbnail, Spinner } from "native-base";
 import AppHeader from './component/layout/Header';
+
+
 class App extends React.Component {
 
   constructor() {
     super();
-    this.state = [
-      {name:'nama 1',phone:'08123123'},
-      {name:'nama 1',phone:'08123123'},
-      {name:'nama 1',phone:'08123123'},
-      {name:'nama 1',phone:'08123123'},
-      {name:'nama 1',phone:'08123123'},
-      {name:'nama 1',phone:'08123123'},
-      {name:'nama 1',phone:'08123123'},
-      {name:'nama 1',phone:'08123123'},
-      {name:'nama 1',phone:'08123123'},
-      {name:'nama 1',phone:'08123123'},
-      {name:'nama 1',phone:'08123123'},
-      {name:'nama 1',phone:'08123123'},
-      {name:'nama 1',phone:'08123123'},
-      {name:'nama 1',phone:'08123123'},
-      {name:'nama 1',phone:'08123123'},
-      {name:'nama 1',phone:'08123123'},
-
-    ]
-
+    this.state = {
+      employees: []
+    }
   }
-
 
   componentDidMount(){
-    return data = this.state.map((data) =>
-      <ListItem avatar>
-        <Left>
-          <Thumbnail source={{ uri: 'https://mhcd.org/wp-content/uploads/2017/12/placeholder-man.png' }} />
-        </Left>
-        <Body>
-          <Text>{data.name}</Text>
-          <Text note>{data.phone}</Text>
-        </Body>
-        <Right>
-          <Text note>3:43 pm</Text>
-        </Right>
-      </ListItem>
-    );
+
+    axios.get('http://7ea838cf.ngrok.io/sitsby/list.json')
+      .then(res => {
+        const resData = res.data.data;
+        // console.log(resData);
+        this.setState({employees:resData});
+    })
+
+    if(this.state.employees.length > 0){
+      return data = this.state.employees.map((employee) =>
+        <ListItem avatar key={employee.id}>
+          <Left>
+            <Thumbnail source={{ uri: 'https://mhcd.org/wp-content/uploads/2017/12/placeholder-man.png' }} />
+          </Left>
+          <Body>
+            <Text>{employee.name}</Text>
+            <Text note>{employee.phone}</Text>
+          </Body>
+        </ListItem>
+
+      );
+    }else{
+      return(
+          <Spinner color='blue' style={{margin:100}}/>
+      )
+    }
   }
-
-
-
 
   render() {
     const listdata = this.componentDidMount();
     return (
       <Container>
         <AppHeader/>
-          <Item regular style={{margin:10}}>
-            <Input placeholder='Search By Name'/>
-          </Item>
           <Content>
               <List>
                 {listdata}
--- component/layout/Header.js
+++ component/layout/Header.js
@@ -8,9 +8,12 @@ class AppHeader extends Component {
             <Title style={{marginLeft:16}} >SITSBY</Title>
           </Body>
           <Right>
-            <Button transparent style={{marginRight:10}}>
-              <Icon name='more' />
+            <Button transparent>
+              <Icon name='search' />
             </Button>
+            <Button transparent style={{marginRight:8}}>
+              <Icon name='refresh' />
+            </Button>            
           </Right>
         </Header>   
     );
